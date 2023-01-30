8394cdf00762f0e0ff3cbde508c68e52e506cc9d
Alan Martinik <alanmartinik@gmail.com>
2023-01-30 19:59:15 -0300

    Primer Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//COTIZADOR SEGUN LA CANTIDAD DE LITROS DE LA PILETA SE MOSTRARA EL EQUIPO IDEAL
+//Se informara la sal a comprar
+//Se informara el precio del equipo (constante en dolares x tipo de cambio de la fecha)
+//Descuento si es usuario 
+//Precio en cuotas (segun precio y con sus intereses correspondientes)
+//Vida util del equipo solo usando en verano
+//Vida util del equipo usandolo todo el año
+// Cantidad de horas prendido en verano y en invierno
+//Cantidad de cloro que evita a usar
+//Cantidad de horas que debe filtrarse cualquier pileta
+
+
+
+console.log("Bienvenido al Cotizador On-Line de Piscina Natural");
+console.log("Ingrese los siguientes Datos");
+
+let nombre = prompt("Ingrese su Nombre: ");
+let mail = prompt ("Ingrese su email: ");
+let telefono = prompt ("Ingrese su numero de telefono");
+
+console.log("Bienvenido: " , nombre);
