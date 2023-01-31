b6484c3f6d453d0e4a3e3031409ad26bb22858c0
Alan Martinik <alanmartinik@gmail.com>
2023-01-31 17:28:17 -0300

    Primer pre-entrega

--- js/script.js
+++ js/script.js
@@ -11,11 +11,67 @@
 
 
 
-console.log("Bienvenido al Cotizador On-Line de Piscina Natural");
-console.log("Ingrese los siguientes Datos");
+alert("Bienvenido al Cotizador On-Line de Piscina Natural");
+alert("Ingrese los siguientes Datos");
 
-let nombre = prompt("Ingrese su Nombre: ");
-let mail = prompt ("Ingrese su email: ");
-let telefono = prompt ("Ingrese su numero de telefono");
+let nombre = prompt("Ingrese su nombre: ");
+let email = prompt("Ingrese su email: ");
+let telefono = prompt("Ingrese su Telefono: ");
+    console.log (nombre);
+    console.log (email);
+    console.log (telefono);
+    ingreso = prompt("ingrese AVANZAR para continuar");
+    console.log("Bienvenido: " , nombre);
 
-console.log("Bienvenido: " , nombre);
+while (ingreso == "AVANZAR"){ 
+    let cotizacion = prompt ("Ingrese COTIZADOR para iniciar o SALIR para finalizar");
+
+
+if(cotizacion == "COTIZADOR"){
+    let largo = parseFloat(prompt ("Ingrese el LARGO de su pileta"));
+    let ancho = parseFloat(prompt ("Ingrese el ANCHO de su pileta"));
+    let prof_min = parseFloat(prompt ("Ingrese la Profundidad MINIMA de su pileta"));
+    let prof_max = parseFloat(prompt ("Ingrese la Profundidad MAXIMA de su pileta"));
+
+    let prof_prom = (prof_max + prof_min) /2;
+
+    console.log("El largo que ingreso es: " , largo);
+    console.log("El ancho que ingreso es: " , ancho);
+    console.log("La profundidad promedio que ingreso es: " , prof_prom);
+    
+
+    let litros_cliente = (largo * ancho * prof_prom) * 1000;
+    console.log("Los litros aprox de su pileta son: " , litros_cliente);
+    console.log (equipo_ideal(litros_cliente));
+
+
+}
+
+else if ( cotizacion == "SALIR" ){
+
+    console.log ("Lo esperamos pronto con los datos solicitados")
+    break
+}
+else
+    console.log("ingrese COTIZADOR o SALIR")
+
+}
+
+
+alert ("cuando guste lo esperamos nuevamente, Gracias!")
+
+
+function equipo_ideal (litros_cliente){
+
+    if (litros_cliente <= 45000){
+        return "El equipo ideal para usted es el SUBMARINO POOL XPERT"
+    }
+    else if ((litros_cliente > 45000) && (litros_cliente <= 60000)){
+        return "El equipo ideal para usted es el CLORADOR SALINO SCC MINI 20grs"
+    }
+    else if (litros_cliente > 6000){
+        return "El equipo ideal para usted es el CLORADOR SALINO SCC 45grs"
+    }
+
+
+}
